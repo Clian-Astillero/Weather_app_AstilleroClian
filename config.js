52b538677ee9bd3ea4f41df4a660e760ec0f8724
Clian-Astillero <clianastillero6@gmail.com>
2022-12-27 18:39:29 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "94a936dd572050c08865abfe05817406"
+    }
+}
+
+module.exports = constants;
